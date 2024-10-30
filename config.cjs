a635ab6182058a9d0fa9e6cd9293763ac8f86262
SPEEDZ-40405 <princechimaspeedz@gmail.com>
2024-10-30 19:07:03 +0100

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -19,8 +19,8 @@ const config = {
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
-  OWNER_NAME: process.env.OWNER_NAME || "Dark-Love-MD",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "2348122441244",
+  OWNER_NAME: process.env.OWNER_NAME || "《°ŠPËĔÐŹ°》",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "2348029378586",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
